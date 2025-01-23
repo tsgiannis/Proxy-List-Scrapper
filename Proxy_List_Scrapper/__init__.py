e8f91875535427495be38a376f73cfaedfddeba9
John Tsioumpris <tsgiannis@gmail.com>
2025-01-23 09:57:54 +0200

    Update __init__.py
    Since I do a lot of scraping I want to check if some Categories are more acceptable than others

--- Proxy_List_Scrapper/__init__.py
+++ Proxy_List_Scrapper/__init__.py
@@ -1,7 +1,8 @@
 """
-    Date: 15-05-2020
-    Created by Sameer Narkhede
-    Project : Proxy-List-Scrapper
+ Date: 15-05-2020
+ Created by Sameer Narkhede
+ Project : Proxy-List-Scrapper
+ 23-01-2025 Some minor modification in order to check if Categories matter
 """
 
 import sys
@@ -28,14 +29,13 @@ ALL = 'ALL'
 
 
 class ScrapperException(BaseException):
-    pass
+     pass
 
 
 class Proxies(object):
     """
-       Proxies is the response data type of getProxies function
+    Proxies is the response data type of getProxies function
     """
-
     def __init__(self, proxies, category):
         """
         Initialize the proxies class
@@ -49,49 +49,50 @@ class Proxies(object):
 
 class Proxy(object):
     """
-        Proxy is the class for proxy.
+    Proxy is the class for proxy.
     """
-
-    def __init__(self, ip, port):
+    def __init__(self, category, ip, port):
         """
         Initialization of the proxy class
         :param ip: ip address of proxy
         :param port: port of proxy
         """
+        self.category = category
         self.ip = ip
         self.port = port
 
 
 class Scrapper:
     """
-    Scrapper class is use to scrape the proxies from various websites.
+     Scrapper class is use to scrape the proxies from various websites.
     """
+
 
     def __init__(self, category='ssl', print_err_trace=True):
         """
-        Initialization of scrapper class
+         Initialization of scrapper class
         :param category: Category of proxy to scrape.
         :param print_err_trace: (True or False) are you required the stack trace for error's if they occured in the program
         """
-        # init with Empty Proxy List
+        #    init with Empty Proxy List
         self.proxies = []
         self.category = category
         self.Categories = {
-            'SSL': SSL,
-            'GOOGLE': GOOGLE,
-            'ANANY': ANANY,
-            'UK': UK,
-            'US': US,
-            'NEW': NEW,
-            'SPYS.ME': SPYS_ME,
-            'PROXYSCRAPE': PROXYSCRAPE,
-            'PROXYNOVA': PROXYNOVA,
-            'PROXYLIST_DOWNLOAD_HTTP': PROXYLIST_DOWNLOAD_HTTP,
-            'PROXYLIST_DOWNLOAD_HTTPS': PROXYLIST_DOWNLOAD_HTTPS,
-            'PROXYLIST_DOWNLOAD_SOCKS4': PROXYLIST_DOWNLOAD_SOCKS4,
-            'PROXYLIST_DOWNLOAD_SOCKS5': PROXYLIST_DOWNLOAD_SOCKS5,
-            'ALL': ALL
-        }
+             'SSL': SSL,
+             'GOOGLE': GOOGLE,
+             'ANANY': ANANY,
+             'UK': UK,
+             'US': US,
+             'NEW': NEW,
+             'SPYS.ME': SPYS_ME,
+             'PROXYSCRAPE': PROXYSCRAPE,
+             'PROXYNOVA': PROXYNOVA,
+             'PROXYLIST_DOWNLOAD_HTTP': PROXYLIST_DOWNLOAD_HTTP,
+             'PROXYLIST_DOWNLOAD_HTTPS': PROXYLIST_DOWNLOAD_HTTPS,
+             'PROXYLIST_DOWNLOAD_SOCKS4': PROXYLIST_DOWNLOAD_SOCKS4,
+             'PROXYLIST_DOWNLOAD_SOCKS5': PROXYLIST_DOWNLOAD_SOCKS5,
+             'ALL': ALL
+         }
         self.print_trace = print_err_trace
 
     def getProxies(self):
@@ -110,9 +111,8 @@ class Scrapper:
             self.category = 'ALL'
             self.filter_proxies_remove_duplicates()
         else:
-            self.proxies = self._get()
-
-        self.proxies = [Proxy(proxy.split(':')[0], proxy.split(':')[1]) for proxy in self.proxies]
+             self.proxies = self._get()
+        self.proxies = [Proxy(proxy['category'],proxy['proxy'].split(':')[0], proxy['proxy'].split(':')[1]) for proxy in self.proxies]
         return Proxies(proxies=self.proxies, category=self.category)
 
     def _get(self):
@@ -131,7 +131,7 @@ class Scrapper:
                 self.proxies = [sub(r"\'\)\;</script>\s*</abbr>\s*</td>\s*<td\salign=\"left\">\s*", ":", m) for m in
                                 matches]
             elif self.category in {'PROXYLIST_DOWNLOAD_HTTP', 'PROXYLIST_DOWNLOAD_HTTPS',
-                                 'PROXYLIST_DOWNLOAD_SOCKS4', 'PROXYLIST_DOWNLOAD_SOCKS5'}:
+                                   'PROXYLIST_DOWNLOAD_SOCKS4', 'PROXYLIST_DOWNLOAD_SOCKS5'}:
                 matches = findall(r'\d+\.\d+\.\d+\.\d+</td>\s*<td>\d+', r.text)
                 self.proxies = [sub(r"</td>\s*<td>", ":", m) for m in matches]
             else:
@@ -144,14 +144,29 @@ class Scrapper:
                 print(traceback.format_exc())
             return []
 
+
+
     def filter_proxies_remove_duplicates(self):
         """
         filter_proxies_remove_duplicates() is the filter for the proxy list. To get the unique proxies it just get
         the LIST of proxies from self object convert it to SET and then convert to LIST.
-
         :return: Update the UNIQUE LIST of proxies.
         """
-        self.proxies = list(set(self.proxies))
+
+        unique_proxies_set = set()
+        unique_proxies = []
+        for proxy in self.proxies:
+         # Ensure the item is a dictionary and has the key 'proxy'
+            if isinstance(proxy, dict) and 'proxy' in proxy:
+                proxy_address = proxy['proxy']
+                if proxy_address not in unique_proxies_set:
+                    unique_proxies_set.add(proxy_address)
+                    unique_proxies.append(proxy)
+
+
+        self.proxies = unique_proxies
+
+
 
 
 __author__ = "Sameer Narkhede"
@@ -160,31 +175,31 @@ __license__ = "MIT LICENCE"
 __version__ = "0.1.0"
 
 if __name__ == "__main__":
-    # By default set ALL for the parameter to get ALL Proxies
-    Category = 'ALL'
+     # By default set ALL for the parameter to get ALL Proxies
+     Category = 'ALL'
 
-    try:
-        # get an parameter from command line
-        Category = sys.argv[1]
+     try:
+     # get an parameter from command line
+         Category = sys.argv[1]
 
-    except IndexError:
-        print('You didn\'t Specify parameter for script')
+     except IndexError:
+         print('You didn\'t Specify parameter for script')
 
-    # Initialize the Scrapper
-    scrapper = Scrapper(category=Category, print_err_trace=True)
+         # Initialize the Scrapper
+         scrapper = Scrapper(category=Category, print_err_trace=True)
 
-    # Get ALL Proxies According to your Choice
-    data = scrapper.getProxies()
+         # Get ALL Proxies According to your Choice
+         data = scrapper.getProxies()
 
-    # Print These Scrapped Proxies
-    print("Scrapped Proxies:")
-    for item in data.proxies:
-        print('{}:{}'.format(item.ip, item.port))
+         # Print These Scrapped Proxies
+         print("Scrapped Proxies:")
+         for item in data.proxies:
+            print('{}:{}'.format(item.ip, item.port))
 
-    # Print the size of proxies scrapped
-    print("Total Proxies")
-    print(data.len)
+         # Print the size of proxies scrapped
+         print("Total Proxies")
+         print(data.len)
 
-    # Print the Category of proxy from which you scrapped
-    print("Category of the Proxy")
-    print(data.category)
+         # Print the Category of proxy from which you scrapped
+         print("Category of the Proxy")
+         print(data.category)
